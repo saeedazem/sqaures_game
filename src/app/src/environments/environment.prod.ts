68ffa04c398c4585c4a9f3cf8d9152132cfa4f63
saeedazem <73793950+saeedazem@users.noreply.github.com>
2021-01-13 01:25:08 +0200

    Add files via upload

---
+++ src/app/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC9mILM6xhkvkkukTRV6Q-dtv5JtZvro6g",
+    authDomain: "squaresgame-a7b2d.firebaseapp.com",
+    databaseURL: "https://squaresgame-a7b2d-default-rtdb.firebaseio.com",
+    projectId: "squaresgame-a7b2d",
+    storageBucket: "squaresgame-a7b2d.appspot.com",
+    messagingSenderId: "683074467670",
+    appId: "1:683074467670:web:1ee5c15958e04637bd48f7"
+  }
+};
+
+// Include your Firebase configuration details.
---
+++ src/app/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC9mILM6xhkvkkukTRV6Q-dtv5JtZvro6g",
+    authDomain: "squaresgame-a7b2d.firebaseapp.com",
+    databaseURL: "https://squaresgame-a7b2d-default-rtdb.firebaseio.com",
+    projectId: "squaresgame-a7b2d",
+    storageBucket: "squaresgame-a7b2d.appspot.com",
+    messagingSenderId: "683074467670",
+    appId: "1:683074467670:web:1ee5c15958e04637bd48f7"
+  }
+};
